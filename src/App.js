b2c752f1165716d48573af530c005b8654b1c5e5
Andrew112 <andrew.coleman330@gmail.com>
2022-01-17 17:23:00 -0600

    1st comitt

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+
+import React, { Component } from "react";
+
+import FetchData from "./components/FetchData";
+
+class App extends Component {
+  state = {
+    visible: true
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <FetchData />
+      </div>
+    );
+  }
+}
+
+export default App
